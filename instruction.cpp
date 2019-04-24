a2ff924161df6eb26f30b3b66bef2bddbf13610d
pienote <pienote1234567@gmail.com>
2019-04-24 19:53:40 -0400

    Add files via upload

--- instruction.cpp
+++ instruction.cpp
@@ -21,17 +21,6 @@ instruction::instruction(std::string instr, int* d, int* r0, int* r1)
 	set_up_columns();
 }
 
-int* instruction::get_reg0()
-{
-	return reg0;
-}
-
-int* instruction::get_reg1()
-{
-	return reg1;
-}
-
-
 instruction::instruction(std::string instr, int* d, int* r0, int inter)
 {
 	//std::cout << "b" << std::endl;
@@ -62,6 +51,16 @@ instruction::instruction(std::string instr, int* r0, int* r1, std::string name)
 	stall = false;
 	branch = name;
 	set_up_columns();
+}
+
+int* instruction::get_reg0()
+{
+	return reg0;
+}
+
+int* instruction::get_reg1()
+{
+	return reg1;
 }
 
 void instruction::set_up_columns()
